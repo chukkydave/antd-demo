f90ab1ee03d4441cddd9db2de7647389f6b3afcb
Chukwuemeka David <159067957+chuksOfKrownpay@users.noreply.github.com>
2024-11-14 22:43:52 +0100

    configure mounted

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Layout from "@/components/Layout";
 import Checker from "@/components/Checker";
 import ChecksHeader from "@/components/ChecksHeader";
 import CheckerStats from "@/components/CheckerStats";
+
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <div className="items-center justify-items-center gap-6 sm:gap-8 md:gap-16 mt-40 lg:mt-48 px-4 md:px-6 lg:px-0 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 sm:gap-12 md:gap-28 justify-center items-center w-full max-w-4xl mx-auto">
@@ -13,4 +27,4 @@ export default function Home() {
       </main>
     </div>
   );
-};
+}
